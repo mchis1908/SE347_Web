dce4473425175bac068e91dbeda7e03c09a3b2e4
mchis1908 <mchis19.08@gmail.com>
2023-04-17 12:02:55 +0700

    UPDATED

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -2,13 +2,48 @@ import React from 'react'
 import "./SignIn.css"
 import logo from "../../Images/logo.png"
 import { useNavigate } from 'react-router-dom'
+import axios from "axios";
+
 
 
 const SignIn = () => {
+    let data = []
     const navigate = useNavigate()
-    function handleClick() {
-        navigate('/admin/home');
-      }
+
+
+    const fetchApi = async (key) => {
+        try {
+            const res = await axios.get('http://localhost:8000/v1/taikhoan/search/' + key);
+            data = res.data[0];
+        }
+        catch (error) {
+            console.log(error.message)
+        }
+        if (data === undefined) {
+            alert('Tài khoản hoặc mật khẩu không chính xác')
+            return
+        }
+        if (document.getElementById('account').value === data.TENTAIKHOAN &&
+            document.getElementById('password').value === data.MATKHAU &&
+            data.PHANQUYEN === 'admin') {
+            navigate('/admin/home')
+        }
+        else if (document.getElementById('account').value === data.TENTAIKHOAN &&
+            document.getElementById('password').value === data.MATKHAU &&
+            data.PHANQUYEN === 'employee') {
+            navigate('/employee/home')
+        }
+        else {
+            alert('Tài khoản hoặc mật khẩu không chính xác')
+        }
+    }
+
+    function TAIKHOAN() {
+
+        let key = document.getElementById('account').value
+        fetchApi(key)
+
+    }
     return (
         <div className='Sign_In_Wrapper'>
             <section className='SignIn'>
@@ -28,7 +63,7 @@ const SignIn = () => {
                                 <input type="password" name="password" id="password" placeholder="Mật khẩu" />
                             </div>
                         </form>
-                        <button className='signin_btn' onClick={handleClick} ><h3>Đăng nhập</h3></button>
+                        <button className='signin_btn' onClick={TAIKHOAN} ><h3>Đăng nhập</h3></button>
                     </div>
                 </div>
             </section>
